d5b4f813737e3c82d126f4ac4fe1e0c9db48cd56
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2019-09-06 11:45:48 -0700

    Prototype for Storage retries

---
+++ sdk/core/Azure.Core/src/Pipeline/RetryRequestModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core.Http;
+
+namespace Azure.Core.Pipeline
+{
+    public class RetryRequestModifier
+    {
+        public virtual void onBeforeRetryPolicy(HttpPipelineMessage message)
+        {
+
+        }
+    }
+}
