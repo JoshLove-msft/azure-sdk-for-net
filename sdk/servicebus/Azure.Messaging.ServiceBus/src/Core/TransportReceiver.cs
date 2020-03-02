3eb585d1e452e95b74321b1c7384b7df9f181dd4
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-03-01 18:11:11 -0800

    Implement receive deferred messages

--- sdk/servicebus/Azure.Messaging.ServiceBus/src/Core/TransportReceiver.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Core/TransportReceiver.cs
@@ -70,6 +70,35 @@ namespace Azure.Messaging.ServiceBus.Core
         public abstract Task OpenLinkAsync(CancellationToken cancellationToken);
 
         /// <summary>
+        /// Completes a series of <see cref="ServiceBusMessage"/> using a list of lock tokens. This will delete the message from the service.
+        /// </summary>
+        ///
+        /// <param name="receivedMessages">An <see cref="IEnumerable{T}"/> containing the lock tokens of the corresponding messages to complete.</param>
+        /// <param name="cancellationToken"></param>
+        public abstract Task CompleteAsync(
+            IEnumerable<ServiceBusReceivedMessage> receivedMessages,
+            CancellationToken cancellationToken);
+
+        /// <summary>Indicates that the receiver wants to defer the processing for the message.</summary>
+        ///
+        /// <param name="message">The lock token of the <see cref="ServiceBusMessage" />.</param>
+        /// <param name="propertiesToModify">The properties of the message to modify while deferring the message.</param>
+        /// <param name="cancellationToken"></param>
+        ///
+        /// <remarks>
+        /// A lock token can be found in <see cref="ServiceBusReceivedMessage.LockToken"/>,
+        /// only when <see cref="ReceiveMode"/> is set to <see cref="ReceiveMode.PeekLock"/>.
+        /// In order to receive this message again in the future, you will need to save the <see cref="ServiceBusReceivedMessage.SequenceNumber"/>
+        /// and receive it using <see cref="ServiceBusReceiver.ReceiveDeferredMessageAsync(long, CancellationToken)"/>.
+        /// Deferring messages does not impact message's expiration, meaning that deferred messages can still expire.
+        /// This operation can only be performed on messages that were received by this receiver.
+        /// </remarks>
+        public abstract Task DeferAsync(
+            ServiceBusReceivedMessage message,
+            IDictionary<string, object> propertiesToModify = null,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
         /// Get the DateTimeOffset for when the session is locked until.
         /// </summary>
         /// <param name="cancellationToken"></param>
@@ -201,39 +230,58 @@ namespace Azure.Messaging.ServiceBus.Core
             CancellationToken cancellationToken = default);
 
         /// <summary>
-        ///
+        /// Abandons a <see cref="ServiceBusReceivedMessage"/>. This will make the message available again for processing.
         /// </summary>
-        /// <param name="lockTokens"></param>
-        /// <param name="timeout"></param>
-        /// <param name="sessionId"></param>
-        /// <param name="isSessionReceiver"></param>
-        /// <param name="dispositionStatus"></param>
-        /// <param name="propertiesToModify"></param>
-        /// <param name="deadLetterReason"></param>
-        /// <param name="deadLetterDescription"></param>
-        /// <returns></returns>
-        internal abstract Task DisposeMessageRequestResponseAsync(
-            Guid[] lockTokens,
-            TimeSpan timeout,
-            DispositionStatus dispositionStatus,
-            bool isSessionReceiver,
-            string sessionId = null,
+        ///
+        /// <param name="message">The <see cref="ServiceBusReceivedMessage"/> to abandon.</param>
+        /// <param name="propertiesToModify">The properties of the message to modify while abandoning the message.</param>
+        /// <param name="cancellationToken"></param>
+        ///
+        /// <remarks>
+        /// Abandoning a message will increase the delivery count on the message.
+        /// This operation can only be performed on messages that were received by this receiver
+        /// when <see cref="ReceiveMode"/> is set to <see cref="ReceiveMode.PeekLock"/>.
+        /// </remarks>
+        public abstract Task AbandonAsync(
+            ServiceBusReceivedMessage message,
             IDictionary<string, object> propertiesToModify = null,
-            string deadLetterReason = null,
-            string deadLetterDescription = null);
+            CancellationToken cancellationToken = default);
 
         /// <summary>
-        ///
+        /// Moves a message to the deadletter sub-queue.
         /// </summary>
-        /// <param name="lockTokens"></param>
-        /// <param name="outcome"></param>
-        /// <param name="timeout"></param>
-        /// <returns></returns>
-        internal abstract Task DisposeMessagesAsync(
-            IEnumerable<Guid> lockTokens,
-            Outcome outcome,
-            TimeSpan timeout);
+        ///
+        /// <param name="message">The <see cref="ServiceBusReceivedMessage"/> to deadletter.</param>
+        /// <param name="deadLetterReason">The reason for deadlettering the message.</param>
+        /// <param name="deadLetterErrorDescription">The error description for deadlettering the message.</param>
+        /// <param name="propertiesToModify">The properties of the message to modify while deferring the message.</param>
+        /// <param name="cancellationToken"></param>
+        ///
+        /// <remarks>
+        /// A lock token can be found in <see cref="ServiceBusReceivedMessage.LockToken"/>,
+        /// only when <see cref="ReceiveMode"/> is set to <see cref="ReceiveMode.PeekLock"/>.
+        /// In order to receive a message from the deadletter queue, you will need a new <see cref="ServiceBusReceiver"/>, with the corresponding path.
+        /// You can use EntityNameHelper.FormatDeadLetterPath(string) to help with this.
+        /// This operation can only be performed on messages that were received by this receiver.
+        /// </remarks>
+        public abstract Task DeadLetterAsync(
+            ServiceBusReceivedMessage message,
+            string deadLetterReason = default,
+            string deadLetterErrorDescription = default,
+            IDictionary<string, object> propertiesToModify = default,
+            CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Receives a <see cref="IList{Message}"/> of deferred messages identified by <paramref name="sequenceNumbers"/>.
+        /// </summary>
+        /// <param name="sequenceNumbers">An <see cref="IEnumerable{T}"/> containing the sequence numbers to receive.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Messages identified by sequence number are returned. Returns null if no messages are found.
+        /// Throws if the messages have not been deferred.</returns>
+        /// <seealso cref="DeferAsync"/>
+        public abstract Task<IList<ServiceBusReceivedMessage>> ReceiveDeferredMessageBatchAsync(
+            IEnumerable<long> sequenceNumbers,
+            CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Renews the lock on the message. The lock will be renewed based on the setting specified on the queue.
@@ -257,12 +305,5 @@ namespace Azure.Messaging.ServiceBus.Core
         public abstract Task<DateTime> RenewSessionLockAsync(
             string sessionId,
             CancellationToken cancellationToken);
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="timeout"></param>
-        internal abstract Task<ReceivingAmqpLink> GetOrCreateLinkAsync(TimeSpan timeout);
-
     }
 }
